b8ddf9c7b578e9fc08ee2cef5e146bdf58ecca9b
dantilse <dantilse@gmail.com>
2018-09-02 22:29:15 -0500

    set up local development server
    Configure Grunt to run simple task to run a development server

--- README.md
+++ README.md
@@ -4,9 +4,8 @@ Creating a basic mockup for the Safety Walk system
 
 ## Build notes
 
-This system uses Grunt to compile SCSS file into a minified stylesheet. Still working on getting a proper server environment set up for faster development.
+This system uses Grunt to compile SCSS file into a minified stylesheet.
 
-- `$grunt watch`: watches for files to change and recompiles
-- `$grunt connect`: opens localhost://3030 and displays the page
-
-**Note:** need to get livereloading working properly
+- `$grunt`: default task watches for files to change and recompiles
+- `$grunt server`: does the same thing as the default task above
+- `$grunt buildcss`: compiles the CSS stylesheet
--- gruntfile.js
+++ gruntfile.js
@@ -1,103 +1,72 @@
 module.exports = function(grunt) {
   // Load tasks
-  grunt.loadNpmTasks("grunt-contrib-connect");
-  grunt.loadNpmTasks("grunt-contrib-jshint");
-  grunt.loadNpmTasks("grunt-contrib-sass");
-  grunt.loadNpmTasks("grunt-contrib-uglify");
-  grunt.loadNpmTasks("grunt-contrib-watch");
-  grunt.loadNpmTasks("grunt-open");
-  grunt.loadNpmTasks("grunt-serve");
+  grunt.loadNpmTasks('grunt-browser-sync');
+  // grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  // grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON("package.json"),
-    connect: {
-      server: {
-        options: {
-          base: ".", // current directory for 'index.html' is root
-          keepalive: true, // keep the server alive indefinitely
-          open: {
-            target: "http://localhost:3030"
-          },
-          port: 3030, // custom port
-          useAvailablePort: true
-        }
-      }
-    },
-    jshint: {
-      options: {
-        jshintrc: ".jshintrc"
-      },
-      all: ["Gruntfile.js", "assets/js/**/*.js", "!assets/build/app.min.js"]
-    },
-    open: {
-      server: {
-        url: "http://localhost:<%= connect.options.port %>"
-      }
-    },
+    pkg: grunt.file.readJSON('package.json'),
+    // jshint: {
+    //   options: {
+    //     jshintrc: ".jshintrc"
+    //   },
+    //   all: ["Gruntfile.js", "assets/js/**/*.js", "!assets/build/app.min.js"]
+    // },
     sass: {
       dist: {
         options: {
-          style: "compressed",
+          style: 'compressed',
           compass: false,
           sourcemap: false
         },
         files: {
-          "build/app.min.css": ["scss/app.scss"]
+          'build/app.min.css': ['scss/app.scss']
         }
       }
     },
-    serve: {
-      options: {
-        port: 9000
-      }
-    },
-    uglify: {
-      dist: {
-        files: {
-          "build/app.min.js": ["js/**/*.js"]
-        },
-        options: {
-          sourceMap: "build/app.min.js.map",
-          sourceMappingURL: "build/app.min.js.map"
-        }
-      }
-    },
-    watch: {
-      options: {
-        livereload: 35729
-      },
-      css: {
-        files: ["scss/*.scss"],
-        tasks: ["sass"]
-      }
-    }
-    // watch: {
-    //   livereload: {
-    //     css: {
-    //       files: [
-    //         "{,*/}*.{html,css,png,jpg,gif,svg}"
-    //       ],
-    //       tasks: ['sass']
+    // uglify: {
+    //   dist: {
+    //     files: {
+    //       "build/app.min.js": ["js/**/*.js"]
     //     },
     //     options: {
-    //       livereload: 37529
+    //       sourceMap: "build/app.min.js.map",
+    //       sourceMappingURL: "build/app.min.js.map"
     //     }
     //   }
-    // }
+    // },
+    watch: {
+      css: {
+        files: ['scss/*.scss'],
+        tasks: ['sass']
+      }
+    },
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src: ['build/*.css', '*.html']
+        }
+      },
+      options: {
+        watchTask: true,
+        server: './'
+      }
+    }
   });
 
   // Register tasks
-  grunt.registerTask("default", ["sass", "uglify"]);
-
-  grunt.registerTask("buildcss", "Compile stylesheet", ["sass"]);
-
   grunt.registerTask(
-    "server",
-    "Open static server and live reload on localhost",
-    ["connect:livereload", "open", "watch"]
+    'default',
+    'The default Grunt task starts up a server on localhost:3000 and watches for style changes',
+    ['browserSync', 'watch']
   );
 
-  grunt.registerTask("test", ["connect", "serve", "watch"]);
+  grunt.registerTask('buildcss', 'Compile stylesheet', ['sass']);
 
-  grunt.registerTask("dev", ["connect", "serve", "watch"]);
+  grunt.registerTask('server', 'Task to run server on localhost:3000', [
+    'browserSync',
+    'watch'
+  ]);
 };
